f086eacf7bfbc3dedcbe42a697da67b75feac8e0
xzAleXzx <xhardxhardx@gmail.com>
2024-06-21 14:20:12 +0500

    Revert "add dist"
    This reverts commit b4278e4c2d336371f84de637eb4373d61d5d6161.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-mrvk-landing",
   plugins: [
     vue(),
   ],
@@ -13,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: "/vue-mrvk-landing"
 })
